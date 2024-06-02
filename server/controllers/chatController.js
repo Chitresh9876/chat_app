218966c3b7e83d33b60ad6fe2ecda739ebfe787b
Chitresh Kumar Gupta <104379067+Chitresh9876@users.noreply.github.com>
2024-06-02 22:07:57 +0530

    created add and remove user from group api

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -69,3 +69,116 @@ export const fetchChats = asyncHandler(async (req, res) => {
            res.status(400).send(err?.message);
     })
 });
+
+
+export const createGroupChat = asyncHandler(async (req, res) => {
+    if (!req.body?.users || !req.body?.name) {
+        res.status(400).json({
+            success: false,
+            message: "Fill all the details",
+        });
+    }
+   
+    var users = JSON.parse(req?.body?.users);
+
+    if (users.length < 2) {
+        res.status(401).json({
+            success: false,
+            message: "Add more than 2 users to create group",
+        });
+    }
+
+    users.push(req.user);
+
+    try {
+        const groupChat = await Chat.create({
+            chatName: req?.body?.name,
+            users: users,
+            isGroupChat: true,
+            groupAdmin: req?.user,
+        });
+
+        const fullGroupChat = await Chat.findOne({ _id: groupChat?._id })
+            .populate("users", "-password")
+            .populate("groupAdmin", "-password");
+        
+        res.status(200).json(fullGroupChat);
+    }
+    catch (err) {
+        res.status(400);
+        throw new Error(err?.message);
+    }
+});
+
+
+export const renameGroup = asyncHandler(async (req, res) => {
+    const { chatId, chatName } = req.body;
+    
+    const updatedChat = await Chat.findByIdAndUpdate(
+        chatId,
+        {
+            chatName,
+        },
+        {
+            new: true,
+        }
+    )
+        .populate("users", "-password")
+        .populate("groupAdmin", "-password");
+    
+    if (!updatedChat) {
+        res.status(400);
+        throw new Error("Chat not found");
+    }
+    else {
+        res.status(200).json(updatedChat);
+    }
+});
+
+
+export const addToGroup = asyncHandler(async (req, res) => {
+    const { chatId, userId } = req.body;
+    
+    const addedUser = await Chat.findByIdAndUpdate(
+      chatId,
+      {
+        $push: { users: userId },
+      },
+      {
+        new: true,
+      }
+    )
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password");
+
+    if (!addedUser) {
+      res.status(400);
+      throw new Error("Chat not found");
+    } else {
+      res.status(200).json(addedUser);
+    }
+});
+
+
+export const removeFromGroup = asyncHandler(async (req, res) => {
+    const { chatId, userId } = req.body;
+    
+    const deleteUser = await Chat.findByIdAndUpdate(
+      chatId,
+      {
+        $pull: { users: userId },
+      },
+      {
+        new: true,
+      }
+    )
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password");
+
+    if (!deleteUser) {
+      res.status(400);
+      throw new Error("Chat not found");
+    } else {
+      res.status(200).json(addedUser);
+    }
+});
--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
-import { accessChat, fetchChats } from "../controllers/chatController.js";
+import { accessChat, addToGroup, createGroupChat, fetchChats, removeFromGroup, renameGroup } from "../controllers/chatController.js";
 
 
 const router = express.Router();
@@ -8,9 +8,9 @@ const router = express.Router();
 
 router.route('/').post(protect, accessChat);
 router.route('/').get(protect, fetchChats);
-// router.route('/group').post(protect, createGroupChat);
-// router.route('/rename').put(protect, renameGroup);
-// router.route('/groupRemove').put(protect, removeFromGroup);
-// router.route('/groupAdd').put(protect, addToGroup);
+router.route('/group').post(protect, createGroupChat);
+router.route('/rename').put(protect, renameGroup);
+router.route('/groupAdd').put(protect, addToGroup);
+router.route('/groupRemove').put(protect, removeFromGroup);
 
 export default router;
