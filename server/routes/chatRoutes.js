9929170e56df55650f3c8714b97c4882741eac19
Chitresh Kumar Gupta <104379067+Chitresh9876@users.noreply.github.com>
2024-06-02 17:14:12 +0530

    fetch chats api created

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
-import { accessChat } from "../controllers/chatController.js";
+import { accessChat, fetchChats } from "../controllers/chatController.js";
 
 
 const router = express.Router();
 
 
 router.route('/').post(protect, accessChat);
-// router.route('/').get(protect, fetchChats);
+router.route('/').get(protect, fetchChats);
 // router.route('/group').post(protect, createGroupChat);
 // router.route('/rename').put(protect, renameGroup);
 // router.route('/groupRemove').put(protect, removeFromGroup);
